a73c97cdcd73a39e952e654c647ef476be1295b4
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2018-10-28 23:04:13 -0500

    got object into window

---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+//global variables
+var quizQuestions = [];
+var score = 0;
+var answers;
+var question;
+var correct;
+var question_1 = "";
+//functions
+
+// function quizInit(){
+//     const output = []
+
+// quizQuestions.forEach
+
+
+//end quizInit function
+
+// function quizAnswers(){
+
+
+
+// } // end quizAnswers function
+
+// function quizReset(){
+
+
+
+
+
+// end quizReset function
+//end functions
+
+// $(document).ready(function(){
+
+
+
+
+
+ quizQuestions = [
+    question_1 = {
+        question:"Each year, the Chicago river is dyed what color to celebrate St. Patricks day?",
+        answers: "a. orange b. green c it dosen't need to be dyed it is beautiul the way it is d. purple",
+        correct: "b"
+
+
+    }
+]
+$("#quizHolder").append(question_1.question);
+ //an array of question objects
+// for (var i =0; i< quizQuestions.length; i++);
+// {
+// var response = prompt(quizQuestions[i]);
+// if (response === quizQuestions[i].answers){
+// score ++;
+// alert ("correct");
+// }// end if statement 
+// else {
+// alert("incorrect")
+// } // end else statement 
+// }//end for loop
+
+// quizInit();
+
+// });// end document. ready function 
