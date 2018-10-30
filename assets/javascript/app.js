e0e80d1b6b28c9e4090f4efbf01991491a5ddc2f
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-10-30 11:45:03 -0500

    a working countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,60 @@
 //global variables
-var quizQuestions = [];
+
 var score = 0;
 var answers= [];
-var question;
-var correct;
-var question_1 = "";
+var correct= 0;
+var incorrect = 0; 
+var useranswer 
+var timer =0
+var clock = 30
+
+var quizQuestions = [
+    question_1 = {
+        question:"Each year, the Chicago river is dyed what color to celebrate St. Patricks day?",
+        answers: ["a. orange", "b. green", "c. it dosen't need to be dyed it is beautiul the way it is", "d. purple"],
+        correct: "b. green"
+
+
+    }
+]
+
+
 //functions
 
-// function quizInit(){
-//     const output = []
+function countDown(){
+    var clock = 30;
+   var timer = setInterval(function() {
+        clock--;
+       $('#timeLeft').html(clock);
+        if (clock === 30) {
+          $('#timeLeft').html(clock - 1);
+        } else if(clock <= 0) {
+           clearInterval(timer);
+           $("#timeLeft").html("times up!");
+ }
+ console.log("I am the clock "+ clock);
+      }, 1000);
+console.log("hello")
+}//end countDown function
 
-// quizQuestions.forEach
+function quizInit(){
+$("#startButton").on("click", countDown())
+$("#quizStart").html("<h2>Click start to test your knowledge!</h2>")
 
 
-//end quizInit function
+}//end quizInit function
 
-// function quizAnswers(){
+ function checkAnswers(){
+     $("input").on("click", function(){
+      $("#test").html ($("input:checked").val());
+      var useranswer = $(this).val();
+    
+    console.log("this is the user answer " + useranswer);
+});
 
+ }
 
-
-// } // end quizAnswers function
+  // end quizAnswers function
 
 // function quizReset(){
 
@@ -30,26 +65,25 @@ var question_1 = "";
 // end quizReset function
 //end functions
 
-// $(document).ready(function(){
+ $(document).ready(function(){
 
 
 
+quizInit();
 
-
- quizQuestions = [
-    question_1 = {
-        question:"Each year, the Chicago river is dyed what color to celebrate St. Patricks day?",
-        answers: ["a. orange", "b. green", "c. it dosen't need to be dyed it is beautiul the way it is", "d. purple"],
-        correct: 2
-
-
-    }
-]
+//puts first quiz question on to page, iterates through array of answers and prints them with select buttons
 $("#quizHolder").append(question_1.question);
 for(var i = 0; i < question_1.answers.length; i++){
 
-$("#answerHolder").append("<label>"+ "<input type= radio>"+question_1.answers[i]+"</label>")}
- //an array of question objects
+$("#answerHolder").append($('<input type="radio" name="greetings" value="' + question_1.answers[i] + '" checked>' + question_1.answers[i] + '</input><br />'));
+}
+checkAnswers();
+if (useranswer===question_1.correct){
+    correct++
+}
+console.log("this is the user answer" + useranswer);
+console.log("the correct answer is " + question_1.correct);
+//an array of question objects
 // for (var i =0; i< quizQuestions.length; i++);
 // {
 // var response = prompt(quizQuestions[i]);
@@ -64,4 +98,4 @@ $("#answerHolder").append("<label>"+ "<input type= radio>"+question_1.answers[i]
 
 // quizInit();
 
-// });// end document. ready function 
+ });// end document. ready function 
