fe0b69508720cb1484a0bf7f297818efa8622056
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-11-01 17:52:35 -0500

    questions are populating on screen. on clicks are in the correct place.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,30 @@
 //global variables
 
 var score = 0;
-var answers= [];
-var correctAns= 0;
-var incorrectAns = 0; 
+var answers = [];
+var correctAns = 0;
+var incorrectAns = 0;
 var useranswer
 var questionContainer
-var currentQuestion = 0; 
+var currentQuestion = 0;
 var setIntervalID;
-var timer =0
+var setTimeoutID;
+var timer = 0
 var clock = 31
 var arrayIndex = 0
-
+var userClicks=false;
 var init
 var quizQuestions = [
     //#0 in the quizQuestions array
-       {
-        question:"Each year, the Chicago river is dyed what color to celebrate St. Patricks day?",
+    {
+        question: "Each year, the Chicago river is dyed what color to celebrate St. Patricks day?",
         answers: ["a. orange", "b. green", "c. it dosen't need to be dyed it is beautiul the way it is", "d. purple"],
-        correct: "b. green",
-
-
+        correct: "b. green"
     },
 
-   //#1 in the quizQuestions array
-   {
-        question:"What animal cursed the Chicago cubs baseball team until they won the World Series 2016?",
+    //#1 in the quizQuestions array
+    {
+        question: "What animal cursed the Chicago cubs baseball team until they won the World Series 2016?",
         answers: ["a. fox", "b. pony", "c. squid", "d. goat"],
         correct: "d. goat",
 
@@ -34,7 +33,7 @@ var quizQuestions = [
 
     //#3 in the quizQuestions array
     {
-        question:"Besides deep dish pizza, what cuisine is Chicago famous for?",
+        question: "Besides deep dish pizza, what cuisine is Chicago famous for?",
         answers: ["a. Hot dogs", "b. Italian beef sandwiches", "c. Brownies", "d. All of the above"],
         correct: "d. goat",
     }
@@ -43,81 +42,86 @@ var quizQuestions = [
 
 //functions
 //makes the clock count down 
-function countDown(){
+function countDown() {
     var clock = 5;
-    var timer = setInterval(function() {
+    var timer = setInterval(function () {
         clock--;
-       $('#timeLeft').html(clock);
+        $('#timeLeft').html(clock);
         if (clock === 5) {
-          $('#timeLeft').html(clock - 1);
-        } else if(clock <= 0) {
-           clearInterval(timer);
-           $("#timeLeft").html("times up!");
- }
- console.log("I am the clock "+ clock);
-      }, 1000);
-console.log("hello")
+            $('#timeLeft').html(clock - 1);
+        } else if (clock <= 0) {
+            clearInterval(timer);
+            $("#timeLeft").html("times up!");
+        }
+        console.log("I am the clock " + clock);
+    }, 1000);
+    console.log("hello")
 }//end countDown function
 
-function quizInit(){
-   //clicking start button makes the countdown start    
-   $("#startButton").on("click", function(){
-   clearInterval(setIntervalID)
-   setIntervalID = setInterval(getQuestion, 10000);
-//    $("#quizStart").html("<h2>Click start to test your knowledge!</h2>")
-   
-   });
-    
+function quizInit() {
+    //clicking start button makes the countdown start    
+    $("#startButton").on("click", function () {
+        getQuestion();
+        clearInterval(setIntervalID)
+        setIntervalID = setInterval(getQuestion, 10000);
+         $("#quizStart").html("<h2>Click start to test your knowledge!</h2>")
+
+    });
+
 }//end quizInit function
 
-function getQuestion(){
-//countDown();
-//"this" references the objects containing the trivia questions. Current question assigns it a value of 0. 
-// so, taking quiz questions making it number 0, and diving into the object to get the question and putting it in 
-// the quizHolder div.  
-$("#quizHolder").html("<h2>"+ quizQuestions[this.currentQuestion].question + "</h2>")
-for (var i = 0; i < quizQuestions[this.currentQuestion].answers.length; i++) {
-    $("#quizHolder").append($("<p><button class = 'answerButton'>" + quizQuestions[currentQuestion].answers[i]  + '</button></p>'));
+function getQuestion() {
+    //"this" references the objects containing the trivia questions. Current question assigns it a value of 0. 
+    // so, taking quiz questions making it number 0, and diving into the object to get the question and putting it in 
+    // the quizHolder div.  
+    $("#quizHolder").html("<h2>" + quizQuestions[this.currentQuestion].question + "</h2>")
+    for (var i = 0; i < quizQuestions[this.currentQuestion].answers.length; i++) {
+        $("#quizHolder").append($("<p><button class='answerbutton'>" + quizQuestions[currentQuestion].answers[i] + '</button></p>'));
 
-}//end for loop inside of getQuestion
-currentQuestion++;
+    }//end for loop inside of getQuestion
+    nextQuestion();
+
 }//end getQuestion function  
 
-function nextQuestion(){
- $(".answerButton").on("click", function(){
-   // setTimeout(getQuestion, 1000); 
-   $("#quizHolder").empty();
-   var clickanswer=  $(this).text().toString().trim()
-   var storeanswer= quizQuestions[currentQuestion].correct.toString().trim();
+function nextQuestion() {
+    $(".answerbutton").on("click", function () {
+        userClicks=true;
+        $("#quizHolder").empty();
+        var clickanswer = $(this).text().toString().trim()
+        var storeanswer = quizQuestions[currentQuestion].correct.toString().trim();
+        console.log("I am the answer the user clicked " + clickanswer);
+        // alert( clickanswer +" - "+  storeanswer +" "+ clickanswer.localeCompare(storeanswer));
+        if (clickanswer === storeanswer) {
+            $("#test").append("You have answered correctly");
+            correctAns++;
+            console.log("score is " + correctAns);
+            
+            clearTimeout(setTimeoutID);
+            setTimeoutID= setTimeout(getQuestion, 3000); 
+        }
+        else{
+            $("#test").append("You have answered incorrectly");
 
-   alert( clickanswer +" - "+  storeanswer +" "+ clickanswer.localeCompare(storeanswer));
-  if (  clickanswer ===storeanswer )
-    {
-       $("#test").append("You have answered correctly");
+        }
 
 
+    });
 
-  }
-
-   currentQuestion++;
-
-});
-
-
+    currentQuestion++;
 }
 
 
 
 
 
- function checkAnswers(){
-     $(".answerButton").on("click", function(){
-         console.log("dosomething")
-    useranswer = $(".answerButton").text();
+function checkAnswers() {
+    $(".answerButton").on("click", function () {
+        console.log("dosomething")
+        useranswer = $(".answerButton").text();
 
-console.log("this is the user answer inside of the  function " + useranswer)
-   
-});
+        console.log("this is the user answer inside of the  function " + useranswer)
+
+    });
 
 }
 
@@ -131,33 +135,32 @@ console.log("this is the user answer " + useranswer);
 
 //end functions
 
- $(document).ready(function(){
+$(document).ready(function () {
 
-checkAnswers();
+    //checkAnswers();
 
- quizInit();
+    quizInit();
 
-nextQuestion();
-//puts first quiz question on to page, iterates through array of answers and displays them with select buttons
-// $("#quizHolder").append(question_1.question);
-// for(var i = 0; i < question_1.answers.length; i++){
+    //nextQuestion();
+    //puts first quiz question on to page, iterates through array of answers and displays them with select buttons
+    // $("#quizHolder").append(question_1.question);
+    // for(var i = 0; i < question_1.answers.length; i++){
 
-// $("#answerHolder").append($('<input type="radio" name="quizButton" value="' + question_1.answers[i] + '" checked>' + question_1.answers[i] + '</input><br />'));
-// }
-//  checkAnswers();
-//  if (useranswer===this.correct){
-//   correctAns++;
-//  }else{
-//    incorrectAns++;
-//  }
- //need an if stataement to validate user answer input with user answer and increment correct if corrrect 
+    // $("#answerHolder").append($('<input type="radio" name="quizButton" value="' + question_1.answers[i] + '" checked>' + question_1.answers[i] + '</input><br />'));
+    // }
+    //  checkAnswers();
+    //  if (useranswer===this.correct){
+    //   correctAns++;
+    //  }else{
+    //    incorrectAns++;
+    //  }
+    //need an if stataement to validate user answer input with user answer and increment correct if corrrect 
     //and increment incorrect 
-console.log("this is the user answer" + useranswer);
-// console.log("the correct answer is " + question_1.correct);
-console.log("you have this many right " + correctAns)
-console.log("you have this many wrong" + incorrectAns)
+    console.log("this is the user answer" + useranswer);
+    // console.log("the correct answer is " + question_1.correct);
+    console.log("you have this many right " + correctAns)
+    console.log("you have this many wrong" + incorrectAns)
 
 
 
- });// end document. ready function 
- 
+});// end document. ready function 
